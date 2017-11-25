c20fb59d94d9ed321d2aef0dd338db42c4b9a933
ChadoNihi <chadoishere@gmail.com>
2017-11-25 16:29:56 +0200

    start implementing search field

--- pages/index-roadmaps/src/js/views/index.js
+++ pages/index-roadmaps/src/js/views/index.js
@@ -18,7 +18,7 @@ export const view = (state, actions) => {
           <div class="level-item">
             <div class="field has-addons">
               <p class="control">
-                <input class="input" type="text" placeholder="Find a post" />
+                <input class="input" type="text" placeholder="Enter one or more search keys" />
               </p>
               <p class="control">
                 <button class="button">
@@ -31,10 +31,9 @@ export const view = (state, actions) => {
 
 <div class="level-right">
   <p class="level-item"><strong>All</strong></p>
-  <p class="level-item"><a>Published</a></p>
-  <p class="level-item"><a>Drafts</a></p>
-  <p class="level-item"><a>Deleted</a></p>
-  <p class="level-item"><a class="button is-success">New</a></p>
+  <p class="level-item"><a>Most relevant</a></p>
+  <p class="level-item"><a>Most demanded</a></p>
+  <p class="level-item"><a class="button is-success">TODO</a></p>
 </div>
 </nav>
 
